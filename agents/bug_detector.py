69e0022da50b399e6db93111d6bc4207b5ac6002
VinayParmar555 <vinayparmar111111@gmail.com>
2026-06-03 18:38:48 +0530

    iteration_3

--- agents/bug_detector.py
+++ agents/bug_detector.py
@@ -32,6 +32,19 @@ def bug_detector(state: PRReviewState):
     """
     
 
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": f"""
+                Diff Analysis:
+                {state.diff_analysis}
+                Raw Diff:
+                {state.pr_diff}
+            """}
+        ]
+    )
+    
     bugs = response.choices[0].message.content
     
     return {
